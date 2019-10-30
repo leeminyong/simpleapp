8c0373b5bc55726683fe86a1e3603e70090d8b6a
aiden <mylee96@gmail.com>
2019-10-30 13:12:34 +0900

    다이얼로그 박스

--- app/src/main/java/com/aiden/andmodule/activity/DialogActivity.java
+++ app/src/main/java/com/aiden/andmodule/activity/DialogActivity.java
@@ -2,92 +2,50 @@ package com.aiden.andmodule.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.graphics.Color;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.aiden.andmodule.LogUtil;
 import com.aiden.andmodule.R;
 
-public class DialogActivity extends AppCompatActivity  {
+public class DialogActivity extends AppCompatActivity {
 
 
-    private Button button;
-
-
+    private DialogBox dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dialog);
-
-        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
-        tabHost1.setup() ;
-
-        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
-        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
-        ts1.setContent(R.id.content1) ;
-        ts1.setIndicator("TAB 1") ;
-        tabHost1.addTab(ts1)  ;
-
-        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
-        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
-        ts2.setContent(R.id.content2) ;
-        ts2.setIndicator("TAB 2") ;
-        tabHost1.addTab(ts2) ;
-
-
-
-        for (int i = 0; i < tabHost1.getTabWidget().getChildCount(); i++) {
-            tabHost1.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#FF0000")); // unselected
-            TextView tv = (TextView) tabHost1.getTabWidget().getChildAt(i).findViewById(android.R.id.title); //Unselected Tabs
-            tv.setTextColor(Color.parseColor("#ffffff"));
-            tv.setTextSize(40);
-        }
-
-        tabHost1.getTabWidget().getChildAt(tabHost1.getCurrentTab()).setBackgroundColor(Color.parseColor("#0000FF")); // selected
-        TextView tv = (TextView) tabHost1.getCurrentTabView().findViewById(android.R.id.title); //for Selected Tab
-        tv.setTextColor(Color.parseColor("#000000"));
-
-        tabHost1.setOnTabChangedListener(tabId -> {
-
-            int id = tabHost1.getCurrentTab();
-
-            if (id == 0) {
-                // your method 1
-                LogUtil.e("DialogActivity","tab  1 ");
-            }
-            else if (id ==1) {
-                // your method 2
-                LogUtil.e("DialogActivity","tab  2 ");
-            }
-
-            for (int i = 0; i < tabHost1.getTabWidget().getChildCount(); i++) {
-                tabHost1.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#FF0000")); // unselected
-                TextView tv2 = (TextView) tabHost1.getTabWidget().getChildAt(i).findViewById(android.R.id.title); //Unselected Tabs
-                tv2.setTextColor(Color.parseColor("#ffffff"));
-            }
-
-            tabHost1.getTabWidget().getChildAt(tabHost1.getCurrentTab()).setBackgroundColor(Color.parseColor("#0000FF")); // selected
-            TextView tv3 = (TextView) tabHost1.getCurrentTabView().findViewById(android.R.id.title); //for Selected Tab
-            tv3.setTextColor(Color.parseColor("#000000"));
-
-        });
-
-
-
+        setContentView(R.layout.act_dialog);
     }
 
+    public void onAlert(View view) {
+        try{
+            dialog = new DialogBox(this, R.style.AppTheme); //여기서 죽는다.(갤5)
+            dialog.show();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    public class DialogBox extends Dialog {
+
+        public DialogBox(Context context,int theme) {
+            super(context,theme);
+            setContentView(R.layout.dialog_alert);
+
+
+            TextView btn_cancel = findViewById(R.id.btn_cancel);
+            btn_cancel.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {dialog.dismiss();	}
+            });
+            TextView btn_ok = findViewById(R.id.btn_ok);
+            btn_ok.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {dialog.dismiss();	}
+            });
+
+        }
+    }
 }
-
-
-
-
