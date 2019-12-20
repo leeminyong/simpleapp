213dd3d751b973159be2fbb4e8e61610a84d0766
leeminyong <mylee96@gmail.com>
2019-12-20 23:56:07 +0900

    수정

---
+++ app/src/main/java/com/aiden/andmodule/activity/RxBindingAct.kt
@@ -0,0 +1,15 @@
+package com.aiden.andmodule.activity
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+import com.aiden.andmodule.R
+
+class RxBindingAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rxbinding)
+    }
+}
